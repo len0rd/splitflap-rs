748b760d8ebafd1ead23808395f49a979930a94b
len0rd <len0rd@users.noreply.github.com>
2023-09-08 23:32:11 -0400

    WIP: hack in mqtt support

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,14 @@ use embedded_graphics::{
     prelude::*,
     text::Text,
 };
+use esp_idf_svc::mqtt::client::{EspMqttClient, EspMqttMessage, MqttClientConfiguration};
+use embedded_svc::mqtt::client::{Event, QoS};
 use esp_idf_svc::{wifi::AsyncWifi, wifi::EspWifi};
 use futures::executor::block_on;
 use mipidsi::Builder;
 use std::thread;
 use std::time::Duration;
+use anyhow::Result;
 
 #[toml_cfg::toml_config]
 pub struct Config {
@@ -35,7 +38,9 @@ pub struct Config {
     wifi_psk: &'static str,
 }
 
-fn main() -> anyhow::Result<()> {
+const DEVICE_UID: &'static str = "splitflap_rs_device0";
+
+fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
@@ -56,7 +61,7 @@ fn main() -> anyhow::Result<()> {
     )
     .expect("Failed to setup AsyncWifi");
 
-    let wifi_connect_result = block_on(wifi::connect_wifi(
+    block_on(wifi::connect_wifi(
         app_config.wifi_ssid,
         app_config.wifi_psk,
         &mut wifi,
@@ -98,15 +103,43 @@ fn main() -> anyhow::Result<()> {
     log::info!("ST7789 initialized");
 
     let style: MonoTextStyle<'_, Rgb565> = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
-    let text = "heyyoo";
-    Text::new(text, Point::new(20, 30), style)
+    Text::new("startup mqtt", Point::new(20, 30), style)
         .draw(&mut display)
         .unwrap();
-
+    
+    let mut mqtt_client = start_mqtt_listener(&app_config)?;
+    
+    mqtt_client.subscribe(&format!("{}/text", DEVICE_UID), QoS::AtLeastOnce)?;
+    
     let mut ii: u8 = 0;
     loop {
         thread::sleep(Duration::from_millis(1000));
+        let counter_str = ii.to_string();
+        let payload: &[u8] = counter_str.as_bytes();
+        mqtt_client.publish(&format!("{}/counter", DEVICE_UID), QoS::AtLeastOnce, true, payload)?;
 
         ii = ii + 1;
     }
+}
+
+fn start_mqtt_listener(cfg: &Config) -> anyhow::Result<EspMqttClient> {
+    // client_id needs to be unique
+    let mqtt_conf = MqttClientConfiguration {
+        client_id: Some("splitflap_display"),
+        keep_alive_interval: Some(Duration::from_secs(120)),
+        ..Default::default()
+    };
+
+    return Ok(EspMqttClient::new(
+        format!("mqtt://{}:{}@{}", cfg.mqtt_user, cfg.mqtt_pass, cfg.mqtt_host),
+        &mqtt_conf,
+        move |msg_event| match msg_event {
+            Ok(Event::Received(msg)) => process_mqtt_msg(msg),
+            _ => log::warn!("Received non-OK from mqtt: {:?}", msg_event),
+        },
+    )?);
+}
+
+fn process_mqtt_msg(msg : &EspMqttMessage) {
+    log::info!("Message from MQTT:: {:?}", msg);
 }
